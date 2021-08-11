5d33bf4b582e13d63cb62a93ac16bd9479cd8486
JoyBoi <JoyBoi@users.noreply.github.com>
2021-08-11 11:06:50 +0530

    Minor Change 3

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -12,13 +12,13 @@
       space-x-6
     "
   >
-    <div class="w-52 flex flex-col justify-between h-48">
+    <div class="w-52 flex flex-col justify-start h-48">
       <img
         class="py-3 h-14 w-10"
         alt="Hitmarker"
         src="../assets/hitmarker2.png"
       />
-      <p class="text-textdark font-light text-sm mb-8">
+      <p class="text-textdark font-light text-xs mt-4 mb-8">
         Open an account in minutes, get full Control for much longer.
       </p>
     </div>
